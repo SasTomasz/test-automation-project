500af4a7264c2ab8d9b0f054c95b85f82b7eba65
Tomasz Sas <tomaszsas.dev@gmail.com>
2024-11-13 18:42:38 +0100

    Add search warning and readiness check to SearchResultPage
    Enhance the SearchResultPage by integrating Selenium's WebDriverWait for efficient synchronization. Implement methods to check page readiness and fetch search warning messages.

--- src/search_result_page.py
+++ src/search_result_page.py
@@ -1,3 +1,18 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 class SearchResultPage:
-    def __init__(self, driver):
+    def __init__(self, driver: WebDriver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 20)
+
+        self._search_warning = (By.XPATH, '//div[contains(text(), "search")]')
+        self._page_title = (By.XPATH, '//h1[text()="Search"]')
+
+    def check_page_is_ready(self):
+        self.wait.until(EC.visibility_of_element_located(self._page_title))
+
+    def get_search_warning(self):
+        return self.driver.find_element(*self._search_warning).text
