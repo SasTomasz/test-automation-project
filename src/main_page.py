a69baaa184e6e467483e35be7b5ee59471e2503d
Tomasz Sas <tomaszsas.dev@gmail.com>
2024-11-13 18:21:08 +0100

    Add full functionality for MainPage class
    Implemented methods to navigate to the main page, fill the search field, click the search button, and verify page readiness using explicit waits. Added imports for WebDriver, WebDriverWait, and expected_conditions to support these features.

--- src/main_page.py
+++ src/main_page.py
@@ -1,21 +1,28 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 class MainPage:
-    def __init__(self, driver):
+    def __init__(self, driver: WebDriver):
+        self.url = "https://www.ministryoftesting.com/"
         self.driver = driver
+        self.wait = WebDriverWait(driver, 20)
+
         self._search_field = (By.ID, "search_query")
         self._search_button = (By.CSS_SELECTOR, "button[aria-label='search']")
         self._sign_in_btn = (By.ID, "nav-sign-in")
 
     def fill_search_field(self, text):
-        pass
+        self.driver.find_element(*self._search_field).send_keys(text)
 
     def click_search_button(self):
-        pass
+        self.driver.find_element(*self._search_button).click()
 
     def go_to_main_page(self):
-        pass
+        self.driver.get(self.url)
 
     def check_the_page_is_ready(self):
-        pass
+        self.wait.until(EC.visibility_of_element_located(self._sign_in_btn))
 
